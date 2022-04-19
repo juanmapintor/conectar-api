9a33ab65926b6a2c49d24ac525e4638835a41018
Juanma Pintor <juanmapintor22@gmail.com>
2022-04-18 22:27:22 -0300

    Implementada InexistenException

--- app/Exceptions/InexistentException.php
+++ app/Exceptions/InexistentException.php
@@ -6,5 +6,8 @@ use Exception;
 
 class InexistentException extends Exception
 {
-    //
+    public function __construct($message = "", $code = 0)
+    {
+        parent::__construct($message, $code);
+    }
 }
